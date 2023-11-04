9d5c55d3e6edfccc1fccbb0cf7eb4cfb16896fbb
Kye <kye@apacmediasolutions.com>
2023-11-04 13:15:01 -0400

    metnet

---
+++ metnet/main.py
@@ -0,0 +1,15 @@
+import torch
+from zeta.nn import Unet  
+
+# Initialize the U-Net model
+model = Unet(n_channels=1, n_classes=2)
+
+# Random input tensor with dimensions [batch_size, channels, height, width]
+x = torch.randn(1, 1, 572, 572)
+
+# Forward pass through the model
+y = model(x)
+
+# Output
+print(f"Input shape: {x.shape}")
+print(f"Output shape: {y.shape}")
